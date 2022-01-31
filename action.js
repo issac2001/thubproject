ecb588388d0070593eb48fbf6d8ae8c375286662
issac2001 <83367715+issac2001@users.noreply.github.com>
2022-01-31 23:31:02 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,85 @@
+function changetowhite() {
+     document.body.style.backgroundColor="white";
+   }
+function changetored() {
+    document.body.style.backgroundColor="rgb(250, 0, 0)";
+  }  
+function changetoyellow() {
+    document.body.style.backgroundColor="rgb(255, 255, 0)";
+  }   
+function changetoblue() {
+    document.body.style.backgroundColor="rgb(0, 225, 255)";
+  }  
+function changetogreen() {
+    document.body.style.backgroundColor="rgb(0, 250, 0)";
+  }  
+function changetoorange() {
+    document.body.style.backgroundColor="rgb(255, 153, 0)";
+  }
+function changetoviolet() {
+    document.body.style.backgroundColor="rgb(195, 0, 255)";
+  }
+function changetodblue() {
+    document.body.style.backgroundColor="rgb(15, 10, 255)";
+  }  
+function changetopink() {
+    document.body.style.backgroundColor="rgb(255, 0, 221)";
+  }    
+function changetodgreen() {
+    document.body.style.backgroundColor="rgb(33, 128, 57)";
+  }     
+function changetodefault() {
+    document.body.style.backgroundColor="rgb(41, 38, 34)";
+  }     
+
+
+
+function headtowhite() {
+        document.getElementById("heading").style.color="white";
+  }
+function headtored() {       
+     document.getElementById("heading").style.color="rgb(250, 0, 0)";
+ }  
+function headtoyellow() {
+        document.getElementById("heading").style.color="rgb(255, 255, 0)";
+ }   
+function headtoblue() {
+        document.getElementById("heading").style.color="rgb(0, 225, 255)";
+ }  
+function headtogreen() {
+        document.getElementById("heading").style.color="rgb(0, 250, 0)";
+ }  
+function headtoorange() {
+        document.getElementById("heading").style.color="rgb(255, 153, 0)";
+ }
+function headtoviolet() {
+        document.getElementById("heading").style.color="rgb(195, 0, 255)";
+ }
+function headtodblue() {
+   document.getElementById("heading").style.color="rgb(15, 10, 255)";
+ }  
+function headtopink() {
+   document.getElementById("heading").style.color="rgb(255, 0, 221)";
+ }    
+function headtodgreen() {
+    document.getElementById("heading").style.color= "rgb(33, 128, 57)";
+ }     
+function headtodefault() {
+   document.getElementById("heading").style.color="floralwhite";
+ }   
+
+function changetosquare() {
+  document.getElementById("shape").style.backgroundColor="orange";
+  document.getElementById("shape").style.borderRadius="0%";
+  document.getElementById("shape").style.width="200px";
+}
+function changetocircle() {
+  document.getElementById("shape").style.backgroundColor="white";
+  document.getElementById("shape").style.borderRadius="50%";
+  document.getElementById("shape").style.width="200px";
+}
+function changetorectangle() {
+  document.getElementById("shape").style.backgroundColor="green";
+  document.getElementById("shape").style.borderRadius="0%";
+  document.getElementById("shape").style.width="400px";
+} 
